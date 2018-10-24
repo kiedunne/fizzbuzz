84a6ec6604b3b339e9cb5b04582eacdaebd570f7
Jackie <kie.dunne@gmail.com>
2018-10-24 15:28:44 +0100

    refactored tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,25 @@
 require 'fizzbuzz'
+numbers = [*1..100]
+
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "buzz" when passed 5' do
+    numbers.each do |n| expect(fizzbuzz(n)).to eq 'buzz' if n % 5 == 0 && n % 3 != 0
+    end
   end
+
+  it 'returns "fizz" when passed 3' do
+    numbers.each do |n| expect(fizzbuzz(n)).to eq 'fizz' if n % 5 != 0 && n % 3 == 0
+    end
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    numbers.each do |n| expect(fizzbuzz(n)).to eq 'fizzbuzz' if n % 5 == 0 && n % 3 == 0
+    end
+  end
+
+  it 'returns "number" when passed 1' do
+    numbers.each do |n| expect(fizzbuzz(n)).to eq n if n % 5 != 0 && n % 3 != 0
+    end
+  end
+
 end
